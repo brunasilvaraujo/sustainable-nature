16b38492396d5f4a6c35cfa91abebf14e7a9554a
Bruna da Silva Araujo <bruna-nascimento_@hotmail.com>
2025-05-16 15:52:05 -0300

    feat: Adiciona ContactSection e estilo

---
+++ src/components/ContactSection.jsx
@@ -0,0 +1,28 @@
+function Contact() {
+    return (
+        <section className="contact">
+            <div className="container">
+                <div className="contact-container">
+                    <div className="contact-content">
+                        <h3 className="contact-title">Entre em contato com a gente!</h3>
+                        <div className="contact-address">
+                            <i className="bi bi-geo-alt"></i>
+                            <p className="contact-text">R. Amauri Souza, 346</p>
+                        </div>
+                        <div className="contact-email">
+                            <i className="bi bi-envelope"></i>
+                            <p className="contact-text">contato@saveplanet.com</p>
+                        </div>
+                        <button className="contact-button">
+                            <i class="bi bi-whatsapp"></i>
+                            Enviar Mensagem
+                        </button>
+                    </div>
+                    <img  className="contact-image" src="/assets/contact.jpg" alt="Contato" />
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Contact;
