b1a0d21a3b5bb2b3d7819ca6a006281d2e7f6c2f
Bruna da Silva Araujo <bruna-nascimento_@hotmail.com>
2025-05-16 14:20:16 -0300

    feat: Adiciona AboutSection

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import Header from './components/Header'
 import Hero from './components/HeroSection'
 import Services from './components/SevicesSection';
+import About from './components/AboutSection';
 
 function App() {
   
@@ -11,6 +12,7 @@ function App() {
       <Header />
       <Hero />
       <Services />
+      <About />
     </div>
   )
 }
---
+++ src/components/AboutSection.jsx
@@ -0,0 +1,22 @@
+function About() {
+    return (
+        <>
+            <section className="about">
+                <img className="about-image" src="/assets/saveThePlanet.jpg" alt="Salvar o Planeta" />
+                <div className="about-content">
+                    <h3 className="about-subtitle">Sobre nós</h3>
+                    <h1 className="about-title">Salvar o Planeta</h1>
+                    <p className="about-description">Junte-se a nós na luta pela preservação do meio ambiente e pela promoção da sustentabilidade.
+                        Nossa missão é conscientizar e educar as pessoas sobre a importância de cuidar do nosso planeta.
+                        Acreditamos que pequenas ações podem fazer uma grande diferença e, juntos, 
+                        podemos criar um futuro mais verde e saudável para todos.
+                        Nossa equipe é composta por apaixonados pela natureza, prontos para compartilhar dicas, 
+                        informações e projetos que visam a proteção do meio ambiente.
+                    </p>
+                </div>
+            </section>
+        </>
+    )
+}
+
+export default About;
